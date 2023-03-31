6daaec8920bd2cc11c37ae3d55923a92dc992f34
Marcus <infinitymdm@gmail.com>
2023-03-31 12:32:49 -0500

    Stub out prototype for new sequential methods

---
+++ characterizer/char_seq_proto.py
@@ -0,0 +1,18 @@
+import numpy as np
+from characterizer.Harness import SequentialHarness
+
+def runSequentialDelaySim():
+    # TODO
+    pass
+
+def runSequentialRecoveryRemovalSim():
+    # TODO
+    pass
+
+def findSetupTime():
+    # TODO
+    pass
+
+def findHoldTime():
+    # TODO
+    pass
