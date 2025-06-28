a9eae95809290dd34b01c039aef55423c8a28fab
ondrej.ille <ondrej.ille@gmail.com>
2025-06-28 15:47:56 +0200

    docs,deps: add SPHINX and Read-the-doc theme to python dependencies.

--- docs/manual/source/conf.py
+++ docs/manual/source/conf.py
@@ -14,7 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
