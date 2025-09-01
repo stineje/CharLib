facdfee9a724512c27183bbf7fa45c6d745e7404
Marcus Mellor <infinitymdm@gmail.com>
2025-09-01 14:20:15 -0500

    WIP: Rewrite libert.library to use Liberty objects

--- charlib/liberty/library.py
+++ charlib/liberty/library.py
@@ -1,149 +1,115 @@
-"""Models liberty library groups"""
+import charlib.liberty.liberty as liberty
 
-from charlib.liberty.UnitsSettings import UnitsSettings
+class Library(liberty.Group):
+    """Convenience class for liberty library groups with pre-populated defaults"""
 
-# TODO: wire loads, operating conditions, power supplies
-
-class Library:
-    """Models a library, which groups cells and their common properties"""
+    # These attributes must be displayed first if present, and in the order below
+    ordered_attributes = [
+        'technology',
+        'delay_model',
+        'bus_naming_style',
+        'date',
+        'comment',
+        'time_unit',
+        'voltage_unit',
+        'leakage_power_unit',
+        'current_unit',
+        'pulling_resistance_unit',
+        'capacitive_load_unit',
+        'revision',
+        'in_place_swap_mode'
+    ]
 
     def __init__(self, name, **attrs):
-        """Create a new Library"""
-        self.name = name
-        self._attrs = attrs
-        self.cells = {}
+        """Construct a library group"""
+        super().__init__('library', name)
+        self.add_attribute('filename', f'{name}.lib')
+        self.add_attribute('technology', 'cmos')
+        self.add_attribute('delay_model', 'table_lookup')
+        self.add_attribute('bus_naming_style', '%s-%d')
+        self.add_attribute('in_place_swap_mode', 'match_footprint')
 
-        # General attributes
-        self.filename = attrs.get('filename', f'{name}.lib')
-        self.technology = 'cmos'
-        self.delay_model = 'table_lookup'
-        self.bus_naming_style = attrs.get('bus_naming_style', '%s-%d')
+        # Add nominal operating conditions
+        self.add_attribute('nom_process', 1.0)
+        self.add_attribute('nom_voltage', 3.3) # FIXME: This isn't a sane default. Needs user input
+        self.add_attribute('nom_temperature', 25.0)
 
-        # Delay and Slew attributes
-        self.input_threshold_pct_rise = attrs.get('input_threshold_pct_rise', 50)
-        self.input_threshold_pct_fall = attrs.get('input_threshold_pct_fall', 50)
-        self.output_threshold_pct_rise = attrs.get('output_threshold_pct_rise', 50)
-        self.output_threshold_pct_fall = attrs.get('output_threshold_pct_fall', 50)
-        self.slew_upper_threshold_pct_rise = attrs.get('slew_upper_threshold_pct_rise', 80)
-        self.slew_lower_threshold_pct_rise = attrs.get('slew_lower_threshold_pct_rise', 20)
-        self.slew_upper_threshold_pct_fall = attrs.get('slew_upper_threshold_pct_fall', 80)
-        self.slew_lower_threshold_pct_fall = attrs.get('slew_lower_threshold_pct_fall', 20)
+        # Override defaults with whatever attrs get passed as kwargs
+        [self.add_attribute(attr_name, value) for attr_name, value in attrs.items()]
 
-        # Units
-        units = attrs.get('units', {})
-        unit_keys = [
-            'time',
-            'voltage',
-            'current',
-            'pulling_resistance',
-            'capacitive_load',
-            'leakage_power',
-            'energy',
-        ]
-        for unit_key in unit_keys:
-            if f'{unit_key}_unit' in attrs:
-                units[unit_key] = attrs[f'{unit_key}_unit']
-        self.units = UnitsSettings(**units)
+        # Copy nom_* attrs into operating_conditions group
+        op_conditions = liberty.Group('operating_conditions', 'typical')
+        op_conditions.add_attribute('process', self.nom_process)
+        op_conditions.add_attribute('voltage', self.nom_voltage)
+        op_conditions.add_attribute('temperature', self.nom_temperature)
 
-        # Nominal operating conditions
-        self.nom_process = attrs.get('nom_process', 1)
-        self.nom_voltage = attrs.get('nom_voltage', 3.3)
-        self.nom_temperature = attrs.get('nom_temperature', 25)
+    def add_lu_table_template(self, lut, **variables):
+        if isinstance(lut, LookupTableTemplate):
+            self.add_group(lut)
+        else:
+            self.add_group(LookupTableTemplate(lut, **variables))
 
     @property
-    def time_unit(self):
-        """Return self.units.time"""
-        return self.units.time
+    def lu_table_templates(self) -> list:
+        """Return lu_table_template groups."""
+        return [group for group in self.groups if group.name == 'lu_table_template']
 
     @property
-    def voltage_unit(self):
-        """Returns self.units.voltage"""
-        return self.units.voltage
+    def cells(self) -> list:
+        """Return cell groups."""
+        return [group for group in self.groups if group.name == 'cell']
 
-    @property
-    def current_unit(self):
-        """Returns self.units.current"""
-        return self.units.current
-
-    @property
-    def pulling_resistance_unit(self):
-        """Returns self.units.resistance"""
-        return self.units.resistance
-
-    @property
-    def capacitive_load_unit(self):
-        """Returns self.units.capacitance"""
-        return self.units.capacitance
-
-    @property
-    def leakage_power_unit(self):
-        """Returns self.units.power"""
-        return self.units.power
-
-    def __getitem__(self, key: str):
-        """Returns self[key]. Searches cells by name."""
-        return self.cells[key]
-
-    @property
-    def attributes(self) -> dict:
-        """Return extra attributes on this library"""
-
-    def add_attribute(self, key: str, value: str):
-        """Add a new liberty attribute to this cell"""
-        self._attrs[key] = str(value)
-
-    def add_cell(self, cell) -> None:
-        """Add a cell to the library"""
-        self.cells[cell.name] = cell
-
-    def templates(self) -> list:
-        templates = []
-        for cell in self.cells.values():
-            templates.extend(cell.templates())
-        return list(set(templates))
-
-    def __str__(self) -> str:
-        """Return str(self)"""
-        spice_unit = lambda unit : unit.prefixed_unit.str_spice()
-        lib_str = [
-            f'library ({self.name}) {{',
-            f'  technology : {self.technology};',
-            f'  delay_model : {self.delay_model};',
-            f'  bus_naming_style : "{self.bus_naming_style}";',
-            '\n  /* Units */',
-            f'  time_unit : "1{spice_unit(self.units.time)}";',
-            f'  voltage_unit : "1{spice_unit(self.units.voltage)}";',
-            f'  current_unit : "1{spice_unit(self.units.current)}";',
-            f'  pulling_resistance_unit : "1{spice_unit(self.units.resistance)}";',
-            f'  leakage_power_unit : "1{spice_unit(self.units.power)}";',
-            f'  capacitive_load_unit : "1{spice_unit(self.units.capacitance)}";',
-            '\n  /* Slew characteristics */',
-            f'  slew_upper_threshold_pct_rise : {self.slew_upper_threshold_pct_rise};',
-            f'  slew_lower_threshold_pct_rise : {self.slew_lower_threshold_pct_rise};',
-            f'  slew_upper_threshold_pct_fall : {self.slew_upper_threshold_pct_fall};',
-            f'  slew_lower_threshold_pct_fall : {self.slew_lower_threshold_pct_fall};',
-            f'  input_threshold_pct_rise : {self.input_threshold_pct_rise};',
-            f'  input_threshold_pct_fall : {self.input_threshold_pct_fall};',
-            f'  output_threshold_pct_rise : {self.output_threshold_pct_rise};',
-            f'  output_threshold_pct_fall : {self.output_threshold_pct_fall};',
-            '\n  /* Operating Conditions */',
-            f'  nom_process : {self.nom_process};',
-            f'  nom_voltage : {self.nom_voltage};',
-            f'  nom_temperature : {self.nom_temperature};',
-        ]
-        # TODO: Display wire loads, operating conditions, and power supplies
-
-        # Display templates from cells
-        lib_str.append('\n  /* Table Templates */')
-        for template in self.templates():
-            for line in template.split('\n'):
-                lib_str.append(f'  {line}')
-
-        # Display cells
-        for name, cell in self.cells.items():
-            lib_str.append(f'\n  /* {name} */')
-            for line in str(cell).split('\n'):
-                lib_str.append(f'  {line}')
-        
-        lib_str.append('}')
+    def to_liberty(self, precision=6):
+        """Convert this library to a Liberty-format string."""
+        # Library display order is specialized
+        lib_str = [f'{self.name}, {self.identifier}{{']
+        for attr in self.ordered_attributes:
+            if hasattr(self, attr):
+                lib_str += [self.attributes[attr].to_liberty(1, precision)]
+        for key, attr in self.attributes.items():
+            if key not in self.ordered_attributes:
+                lib_str += [attr.to_liberty(1, precision)]
+        for group in self.groups:
+            lib_str += group.to_liberty(1, precision).split('\n')
         return '\n'.join(lib_str)
+
+
+class LookupTableTemplate(liberty.Group):
+    """Convenience class for lu_table_template groups, which have slightly unusual formatting"""
+    def __init__(self, lut_name, **variables):
+        """Initialize a lu_table_template group
+
+        :param lut_name: The identifier for the lu_table_template group.
+        :param **variables: keyword arguments consisting of variable names and integer sizes.
+
+        While lut templates are normal groups, they have quite a bit of odd formatting. Indices,
+        for example, are complex attributes with a single long string value of comma-separated
+        floats corresponding to one of the variables. Moreover, we don't care about the values in
+        the indices as they are overridden by tables using these templates.
+        """
+        super().__init__('lu_table_template', lut_name)
+        index = 1
+        for variable, length in variables.items():
+            self.add_attribute(f'variable_{index}', variable)
+            self.add_attribute(f'index_{index}', ['"'+', '.join([str(1.0+i) for i in range(length)])+'"'])
+            index += 1
+
+    def to_liberty(self, indent_level=0, precision=6):
+        # LUT display order is specialized
+        indent = liberty.INDENT_STR * indent_level
+        indices = range(1, len(self.attributes)//2 + 1)
+        lut_str = [f'{indent}{self.name} ({self.identifier}) {{']
+        lut_str += [self.attributes[f'variable_{i}'].to_liberty(indent_level+1, precision) for i in indices]
+        lut_str += [self.attributes[f'index_{i}'].to_liberty(indent_level+1, precision) for i in indices]
+        lut_str += [f'{indent}}}']
+        for group in self.groups.values():
+            group_str += group.to_liberty(indent_level+1, precision).split('\n')
+        return '\n'.join(lut_str)
+
+
+if __name__ == "__main__":
+    # Test Library and LUT classes
+    library = Library('gf180')
+    lut = LookupTableTemplate('delay_template_5x5', total_output_net_capacitance=5, input_net_transition=5)
+    library.add_lu_table_template(lut)
+    print(library.to_liberty())
