ba362c4919eacf4218cbcf781ed816b1503d1a8f
Marcus Mellor <infinitymdm@gmail.com>
2026-02-25 15:25:05 -0600

    charlib/characterizer/logic/functions: Report truth tables & test vectors as {pin: state} dicts

--- charlib/characterizer/logic/functions.py
+++ charlib/characterizer/logic/functions.py
@@ -10,6 +10,17 @@ class Function:
         self.stored_test_vectors = test_vectors
 
     @property
+    def pythonic_expression(self) -> str:
+        """Return a python-executable version of the function's expression"""
+        py_expr = (
+            self.expression
+                .replace('~', ' not ')
+                .replace('&', ' and ')
+                .replace('|', ' or ')
+        )
+        return py_expr
+
+    @property
     def operands(self) -> list:
         """Return a list of operand names"""
         return sorted(set(re.findall(r'(\w+)', self.expression)))
@@ -19,17 +30,17 @@ class Function:
         operands = self.operands
         if not len(inputs) == len(operands):
             raise ValueError(f'Expected {len(operands)} inputs for function {self.expression}, got {len(inputs)}')
-        f = eval(f'lambda {",".join(operands)}: int({self.expression.replace("~", " not ").replace("&", " and ").replace("|", " or ")})')
-        return f(**inputs)
+        callable_expr = eval(f'lambda {",".join(operands)}: int({self.pythonic_expression})')
+        return callable_expr(**inputs)
 
     def truth_table(self) -> list:
         """Return a truth table for this function"""
         table = []
         length = len(self.operands)
         for n in range(2**length):
-            input_vector = [int(c) for c in f'{n:0{length}b}']
-            result = self.eval(**dict(zip(self.operands, input_vector)))
-            table.append([input_vector, result])
+            result = dict(zip(self.operands, [int(c) for c in f'{n:0{length}b}']))
+            result['output'] = self.eval(**result)
+            table.append(result)
         return table
 
     def __eq__(self, other) -> bool:
@@ -44,7 +55,7 @@ class Function:
 
     def __str__(self) -> str:
         """Return str(self)"""
-        return self.expression.replace('~', '!')
+        return self.expression
 
     def __repr__(self) -> str:
         """Return repr(self)"""
@@ -74,24 +85,20 @@ class Function:
         test_vectors = []
         table = self.truth_table()
         while table:
-            row = table.pop(0) # Get top row of table
+            top_row = table.pop(0) # Get top row of table
             # Compare to each later row with a differing output
-            for compared_row in [r for r in table if not r[1] == row[1]]:
+            for compared_row in [cr for cr in table if not cr['output'] == top_row['output']]:
                 # Check if input differs by only one pin
-                delta_row = []
-                delta_count = 0
-                for i, j in zip(row[0], compared_row[0]):
-                    if i == j:
-                        delta_row.append(str(i))
+                deltas = {}
+                for pin in top_row.keys():
+                    if top_row[pin] == compared_row[pin]:
+                        deltas[pin] = str(top_row[pin])
                     else:
-                        delta_row.append(f'{i}{j}')
-                        delta_count += 1
-                if delta_count == 1:
-                    # Add the output to the new test vector
-                    delta_row.append(f'{row[1]}{compared_row[1]}')
-                    test_vectors.append(delta_row)
-                    # Append a second copy with each element reversed
-                    test_vectors.append([s[::-1] for s in delta_row])
+                        deltas[pin] = f'{top_row[pin]}{compared_row[pin]}'
+                if len(''.join(deltas.values())) == len(top_row) + 2:
+                    # Add both the test vector and its reverse
+                    test_vectors.append(deltas)
+                    test_vectors.append({pin: state[::-1] for pin, state in deltas.items()})
         self.stored_test_vectors = test_vectors
         return test_vectors
 
@@ -102,14 +109,15 @@ class StateFunction(Function):
                  preset=None, clear=None):
         """Initialize a function with internal recurrence and optional state-related inputs.
 
-        :param expression:
-        :param state_name:
-        :param clock:
-        :param enable:
-        :param preset:
-        :param clear:
+        :param expression: A boolean expression describing how this output changes state.
+        :param state_name: The name of the state storage element for this output.
+        :param clock: The Cell.Pin with the clock role (if any).
+        :param enable: The Cell.Pin with the enable role (if any).
+        :param preset: The Cell.Pin with the set/preset role (if any).
+        :param clear: The Cell.Pin with the clear/reset role (if any).
         """
-        self.base_expression = str(expression)
+        self.base_expression = expression
+        self.state_variable = state_name
 
         # Add each state-related input cumulatively, if present
         if clock:
@@ -130,7 +138,16 @@ class StateFunction(Function):
 
     def __str__(self) -> str:
         """Return str(self)"""
-        return self.base_expression.replace('~', '!')
+        return str(self.base_expression)
+
+    @property
+    def test_vectors(self) -> list:
+        """Generate valid test vectors, accounting for internal state"""
+        # Start with superclass test vectors, but filter with two criteria:
+        # 1. Remove all TVs where internal state does not match output initial state
+        # 2. Afterwards, remove internal state variable from all test vectors
+        # for tv in super().test_vectors:
+        return super().test_vectors # FIXME
 
 
 def generate_yml(expressions):
