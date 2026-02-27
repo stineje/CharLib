2194287a87b4347eb6ce27d95a661b062dadd4d4
Marcus Mellor <marcus@infinitymdm.dev>
2026-02-26 22:09:59 -0600

    test/logic: Add WIP DFFSR StateFunction test

--- test/logic/test_functions.py
+++ test/logic/test_functions.py
@@ -40,21 +40,73 @@ def test_inverting_dffsr():
 
     This test uses a DFF with posedge clock and active low set & reset.
     """
-    pass # TODO
+    expr = '!D'
+    state_var = 'IQN'
+    clock_pin = Pin('CLK', 'input', role='clock', edge_triggered=True)
+    set_pin = Pin('S', 'input', role='set', inverted=True)
+    reset_pin = Pin('R', 'input', role='reset', inverted=True)
+    function = StateFunction(expr, state_var, clock=clock_pin, preset=set_pin, clear=reset_pin)
+
+    assert(function.base_expression == '~D')
+    assert(function.expression == 'R & (~S | (CLK & (~D) | ~CLK & IQN))')
+    assert(set(function.operands) == {'CLK', 'D', 'IQN', 'S', 'R'})
+
+    expected_truth_table = [
+        {'CLK': 0, 'D': 0, 'IQN': 0, 'S': 0, 'R': 0, Function.OUT: 0},
+        {'CLK': 0, 'D': 0, 'IQN': 0, 'S': 0, 'R': 1, Function.OUT: 1},
+        {'CLK': 0, 'D': 0, 'IQN': 0, 'S': 1, 'R': 0, Function.OUT: 0},
+        {'CLK': 0, 'D': 0, 'IQN': 0, 'S': 1, 'R': 1, Function.OUT: 0},
+        {'CLK': 0, 'D': 0, 'IQN': 1, 'S': 0, 'R': 0, Function.OUT: 0},
+        {'CLK': 0, 'D': 0, 'IQN': 1, 'S': 0, 'R': 1, Function.OUT: 1},
+        {'CLK': 0, 'D': 0, 'IQN': 1, 'S': 1, 'R': 0, Function.OUT: 0},
+        {'CLK': 0, 'D': 0, 'IQN': 1, 'S': 1, 'R': 1, Function.OUT: 1},
+        {'CLK': 0, 'D': 1, 'IQN': 0, 'S': 0, 'R': 0, Function.OUT: 0},
+        {'CLK': 0, 'D': 1, 'IQN': 0, 'S': 0, 'R': 1, Function.OUT: 1},
+        {'CLK': 0, 'D': 1, 'IQN': 0, 'S': 1, 'R': 0, Function.OUT: 0},
+        {'CLK': 0, 'D': 1, 'IQN': 0, 'S': 1, 'R': 1, Function.OUT: 0},
+        {'CLK': 0, 'D': 1, 'IQN': 1, 'S': 0, 'R': 0, Function.OUT: 0},
+        {'CLK': 0, 'D': 1, 'IQN': 1, 'S': 0, 'R': 1, Function.OUT: 1},
+        {'CLK': 0, 'D': 1, 'IQN': 1, 'S': 1, 'R': 0, Function.OUT: 0},
+        {'CLK': 0, 'D': 1, 'IQN': 1, 'S': 1, 'R': 1, Function.OUT: 1},
+        {'CLK': 1, 'D': 0, 'IQN': 0, 'S': 0, 'R': 0, Function.OUT: 0},
+        {'CLK': 1, 'D': 0, 'IQN': 0, 'S': 0, 'R': 1, Function.OUT: 1},
+        {'CLK': 1, 'D': 0, 'IQN': 0, 'S': 1, 'R': 0, Function.OUT: 0},
+        {'CLK': 1, 'D': 0, 'IQN': 0, 'S': 1, 'R': 1, Function.OUT: 1},
+        {'CLK': 1, 'D': 0, 'IQN': 1, 'S': 0, 'R': 0, Function.OUT: 0},
+        {'CLK': 1, 'D': 0, 'IQN': 1, 'S': 0, 'R': 1, Function.OUT: 1},
+        {'CLK': 1, 'D': 0, 'IQN': 1, 'S': 1, 'R': 0, Function.OUT: 0},
+        {'CLK': 1, 'D': 0, 'IQN': 1, 'S': 1, 'R': 1, Function.OUT: 1},
+        {'CLK': 1, 'D': 1, 'IQN': 0, 'S': 0, 'R': 0, Function.OUT: 0},
+        {'CLK': 1, 'D': 1, 'IQN': 0, 'S': 0, 'R': 1, Function.OUT: 1},
+        {'CLK': 1, 'D': 1, 'IQN': 0, 'S': 1, 'R': 0, Function.OUT: 0},
+        {'CLK': 1, 'D': 1, 'IQN': 0, 'S': 1, 'R': 1, Function.OUT: 0},
+        {'CLK': 1, 'D': 1, 'IQN': 1, 'S': 0, 'R': 0, Function.OUT: 0},
+        {'CLK': 1, 'D': 1, 'IQN': 1, 'S': 0, 'R': 1, Function.OUT: 1},
+        {'CLK': 1, 'D': 1, 'IQN': 1, 'S': 1, 'R': 0, Function.OUT: 0},
+        {'CLK': 1, 'D': 1, 'IQN': 1, 'S': 1, 'R': 1, Function.OUT: 0},
+    ]
+    for row in function.truth_table():
+        assert(row in expected_truth_table)
+    assert(len(expected_truth_table) == len(function.truth_table()))
+
+    expected_test_vectors = [
+        {'CLK': 1, 'D': 1, 'IQN': 1, 'S': 1, 'R': 1, Function.OUT: 0},
+    ]
+
 
 def test_noninverting_srlat():
     """Verify truth table and test vectors for the noninverting function of an SR latch"""
     pass # TODO
 
-def test_inverting_dlat()
+def test_inverting_dlat():
     """Verify truth table and test vectors for the inverting function of a D-latch"""
     pass # TODO
 
-def test_and2()
+def test_and2():
     pass # TODO
 
-def test_xor2()
+def test_xor2():
     pass # TODO
 
-def test_addf()
+def test_addf():
     pass # TODO
