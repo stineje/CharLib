0c132d073e732ab8154aca675bb990490d4334f7
Marcus Mellor <infinitymdm@gmail.com>
2026-06-16 11:31:03 -0500

    test/examples: Fix typos, pin pdks to commits, add curl retry flags

--- test/examples/get_gf180mcu_osu_9t.sh
+++ test/examples/get_gf180mcu_osu_9t.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
+export GF180MCU_OSU_SC_COMMIT="8a2f58f283a2eaa725314c9e1b8b7d1d343f23a3"
+export GF180MCU_FD_PR_COMMIT="4adc3a4704fbe722bdf2145341a409b6419788fd"
 mkdir -p gf180mcu/spice
 mkdir -p gf180mcu/models
-curl -s https://raw.githubusercontent.com/stineje/globalfoundries-pdk-libs-gf180mcu_osu_sc/refs/heads/main/gf180mcu_osu_sc_gp9t3v3/spice/gf180mcu_osu_sc_gp9t3v3.spice > gf180mcu/spice/osu_sc_9t.spice
-curl -s https://raw.githubusercontent.com/fossi-foundation/globalfoundries-pdk-libs-gf180mcu_fd_pr/refs/heads/main/models/ngspice/design.spice > gf180mcu/models/design.spice
-curl -s https://raw.githubusercontent.com/fossi-foundation/globalfoundries-pdk-libs-gf180mcu_fd_pr/refs/heads/main/models/ngspice/sm141064.spice > gf180mcu/models/sm141064.spice
+curl -fsSL --retry 3 https://raw.githubusercontent.com/stineje/globalfoundries-pdk-libs-gf180mcu_osu_sc/$GF180MCU_OSU_SC_COMMIT/gf180mcu_osu_sc_gp9t3v3/spice/gf180mcu_osu_sc_gp9t3v3.spice > gf180mcu/spice/osu_sc_9t.spice
+curl -fsSL --retry 3 https://raw.githubusercontent.com/fossi-foundation/globalfoundries-pdk-libs-gf180mcu_fd_pr/$GF180MCU_FD_PR_COMMIT/models/ngspice/design.spice > gf180mcu/models/design.spice
+curl -fsSL --retry 3 https://raw.githubusercontent.com/fossi-foundation/globalfoundries-pdk-libs-gf180mcu_fd_pr/$GF180MCU_FD_PR_COMMIT/models/ngspice/sm141064.spice > gf180mcu/models/sm141064.spice
--- test/examples/get_osu350.sh
+++ test/examples/get_osu350.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
+export OSU035_COMMIT="b36db529c2dff117e1fbead561bf792ec866e1cb"
 mkdir -p osu350/spice
 mkdir -p osu350/models
-curl -s https://raw.githubusercontent.com/stineje/MOSIS_SCMOS/refs/heads/main/latest/cadence/lib/ami035/signalstorm/osu035_stdcells.sp > osu350/spice/osu035_stdcells.sp
-curl -s https://raw.githubusercontent.com/stineje/MOSIS_SCMOS/refs/heads/main/latest/cadence/lib/ami035/lib/ami035.m > osu350/models/ami035.m
-curl -s https://raw.githubusercontent.com/stineje/CharLib/refs/heads/main/test/pdks/osu350/fix_hspice_models.patch > osu350/fix_hspice_models.patch
+curl -fsSL --retry 3 https://raw.githubusercontent.com/stineje/MOSIS_SCMOS/$OSU035_COMMIT/latest/cadence/lib/ami035/signalstorm/osu035_stdcells.sp > osu350/spice/osu035_stdcells.sp
+curl -fsSL --retry 3 https://raw.githubusercontent.com/stineje/MOSIS_SCMOS/$OSU035_COMMIT/latest/cadence/lib/ami035/lib/ami035.m > osu350/models/ami035.m
+curl -fsSL --retry 3 https://raw.githubusercontent.com/stineje/CharLib/refs/heads/main/test/pdks/osu350/fix_hspice_models.patch > osu350/fix_hspice_models.patch
 patch -s osu350/models/ami035.m osu350/fix_hspice_models.patch
