eaad35c77f540129f51b9b61404342b495b2a8a4
Marcus Mellor <infinitymdm@gmail.com>
2026-07-08 15:53:31 -0500

    docs: Add autodoc extension for future integration with Procedure docs

--- docs/manual/source/conf.py
+++ docs/manual/source/conf.py
@@ -15,6 +15,7 @@ release = '2.1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'sphinx.ext.autodoc',
     'sphinxcontrib.mermaid',
 ]
 
