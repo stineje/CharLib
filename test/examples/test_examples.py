435294ce1a5031149b3e16a984a9ee89009e121e
Marcus Mellor <infinitymdm@gmail.com>
2026-07-16 16:44:48 -0500

    test: Add example yaml configs to regression tests

---
+++ test/examples/test_examples.py
@@ -0,0 +1,21 @@
+from charlib.characterizer.characterizer import Characterizer
+from charlib.cli import utils
+
+from pathlib import Path
+
+def run(settings, cells):
+    characterizer = Characterizer(**settings)
+    for name, properties in utils.read_cell_configs(cells):
+        characterizer.add_cell(name, properties)
+    libtext = characterizer.characterize()
+
+    libfile = characterizer.settings.results_dir / characterizer.library.file_name
+    libfile.parent.mkdir(parents=True, exist_ok=True)
+    with open(libfile, 'w') as f:
+        f.write(str(libtext))
+
+def test_examples():
+    for file in utils.find_yaml_files(Path(__file__).resolve().parent):
+        config = utils.find_config(file)
+        run(config['settings'], config['cells'])
+
